24516fe407ee4c832d03aff53c4c7a6087659cb4
colbyfayock <fayock@gmail.com>
2021-05-10 13:33:33 -0400

    Step 3: installing SendGrid mail API and configuring API key

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,4 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
   res.status(200).json({ status: 'Ok' })
