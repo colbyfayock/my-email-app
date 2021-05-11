4afd2154568b879c95d72030bde39f1b2e57e8dd
colbyfayock <fayock@gmail.com>
2021-05-10 20:50:58 -0400

    Step 4: setting up serverless function to programmatically send form data as an email

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,6 +2,22 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default (req, res) => {
-  res.status(200).json({ status: 'Ok' })
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
+
+  await mail.send({
+    to: 'hello@colbyfayock.com',
+    from: 'hello@colbyashi.com',
+    subject: 'New Message!',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  });
+
+  res.status(200).json({ status: 'Ok' });
 }
